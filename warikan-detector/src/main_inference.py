82b04341b82fbbbb39bdc6a26c3a25be7f9dd7a1
Masayuki Anekawa <anekawamasayuki@gmail.com>
2023-08-28 16:19:05 +0900

    use process_csv_for_inference in process_folder_for_inference

--- warikan-detector/src/main_inference.py
+++ warikan-detector/src/main_inference.py
@@ -8,24 +8,12 @@ def process_folder_for_inference(input_folder_path, output_folder_path, model_sa
     # List all CSV files in the folder
     csv_files = [f for f in os.listdir(input_folder_path) if f.endswith('.csv')]
 
-    # Load the model and label encoders
-    clf = load(model_save_path)
-    label_encoders = load(encoder_save_path)
-
     # Process each CSV file
     for csv_file in csv_files:
-        # Load data
-        file_path = os.path.join(input_folder_path, csv_file)
-        data = pd.read_csv(file_path, encoding='SHIFT_JIS')
-        # Preprocess data
-        preprocessed_data = preprocess_data_handle_unseen(data, label_encoders)
-        # Predict
-        predictions = clf.predict(preprocessed_data)
-        # Add predictions to original data
-        data['予測_割り勘対象'] = predictions
+        result = process_csv_for_inference(os.path.join(input_folder_path, csv_file), model_save_path, encoder_save_path)
         # Save to the output folder
         output_file_path = os.path.join(output_folder_path, "predicted_" + csv_file)
-        data.to_csv(output_file_path, index=False, encoding="SHIFT_JIS")
+        result.to_csv(output_file_path, index=False, encoding="SHIFT_JIS")
 
 # Example usage:
 # process_folder_for_inference("TestData", "PredictedData")
