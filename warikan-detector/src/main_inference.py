2f5e05cbee3a44651322c2c7cc8c4dc8da3098b4
Masayuki Anekawa <anekawamasayuki@gmail.com>
2023-08-28 18:38:55 +0900

    Use process_stream_for_inference

--- warikan-detector/src/main_inference.py
+++ warikan-detector/src/main_inference.py
@@ -3,37 +3,44 @@ import pandas as pd
 from joblib import load
 
 from src.preprocessing import preprocess_data_handle_unseen
+from src.warikan_stream import WarikanStream
 
-def process_folder_for_inference(input_folder_path, output_folder_path, model_save_path="models/random_forest_model.pkl", encoder_save_path="models/label_encoders.pkl"):
+def process_folder_for_inference(input_folder_path, output_folder_path, **kwargs) -> None:
     # List all CSV files in the folder
     csv_files = [f for f in os.listdir(input_folder_path) if f.endswith('.csv')]
-
     # Process each CSV file
     for csv_file in csv_files:
-        result = process_csv_for_inference(os.path.join(input_folder_path, csv_file), model_save_path, encoder_save_path)
-        # Save to the output folder
+        input_file_path = os.path.join(input_folder_path, csv_file)
         output_file_path = os.path.join(output_folder_path, "predicted_" + csv_file)
-        result.to_csv(output_file_path, index=False, encoding="SHIFT_JIS")
-
-# Example usage:
-# process_folder_for_inference("TestData", "PredictedData")
+        process_csv_file_for_inference(input_file_path, output_file_path, **kwargs)
 
 
-def process_csv_for_inference(file_path, model_save_path="models/random_forest_model.pkl", encoder_save_path="models/label_encoders.pkl"):
-    # Load the model and label encoders
+def process_csv_file_for_inference(input_file_path, output_file_path, **kwargs) -> None:
+    with open(input_file_path) as input, open(output_file_path, 'w') as output:
+        process_stream_for_inference(input, output, **kwargs)
+
+
+def process_stream_for_inference(input_stream: WarikanStream, output_stream: WarikanStream, **kwargs) -> None:
+    # Load model and label encoders, overriding the default paths if necessary
+    model_save_path = kwargs.get('model_save_path', 'models/random_forest_model.pkl')
     clf = load(model_save_path)
-    label_encoders = load(encoder_save_path)
-
+    encoder_save_path = kwargs.get('encoder_save_path', 'models/label_encoders.pkl')
+    label_encoder = load(encoder_save_path)
     # Load data
-    data = pd.read_csv(file_path, encoding='SHIFT_JIS')
+    data = pd.read_csv(input_stream, encoding='SHIFT_JIS')
     # Preprocess data
-    preprocessed_data = preprocess_data_handle_unseen(data, label_encoders)
+    preprocessed_data = preprocess_data_handle_unseen(data, label_encoder)
     # Predict
     predictions = clf.predict(preprocessed_data)
     # Add predictions to original data
     data['予測_割り勘対象'] = predictions
     # Return the predicted data
-    return data
+    data.to_csv(output_stream, index=False, encoding="SHIFT_JIS")
 
-# Example usage:
-# predicted_data = process_csv_for_inference("TestData/test.csv")
+
+def load_model(model_save_path):
+    return load(model_save_path)
+
+
+def load_label_encoders(encoder_save_path):
+    return load(encoder_save_path)
--- warikan-detector/tests/test_main_inference.py
+++ warikan-detector/tests/test_main_inference.py
@@ -2,8 +2,10 @@ import os
 import shutil
 import tempfile
 import unittest
+from unittest.mock import mock_open, patch
 import pandas as pd
-from src.main_inference import process_folder_for_inference, process_csv_for_inference
+
+from src import main_inference
 
 class TestProcessFolderForInference(unittest.TestCase):
     def setUp(self):
@@ -25,34 +27,55 @@ class TestProcessFolderForInference(unittest.TestCase):
             'ID': ['1', '2', '3'],
         })
         self.test_file_path = os.path.join(self.input_dir, 'test.csv')
+        self.test_file_path_2 = os.path.join(self.input_dir, 'test_2.csv')
         self.test_data.to_csv(self.test_file_path, index=False, encoding='SHIFT_JIS')
+        self.test_data.to_csv(self.test_file_path_2, index=False, encoding='SHIFT_JIS')
+        self.test_output_file_path = os.path.join(self.output_dir, 'predicted_test.csv')
+        self.test_output_file_path_2 = os.path.join(self.output_dir, 'predicted_test_2.csv')
 
     def tearDown(self):
         # Remove the temporary directory and its contents
         shutil.rmtree(self.input_dir)
         shutil.rmtree(self.output_dir)
 
-    def test_process_folder_for_inference(self):
+    @patch.object(main_inference, 'process_csv_file_for_inference')
+    def test_process_folder_for_inference(self, mock_process_csv_file_for_inference):
         # Call the function with the test input and output directories
-        process_folder_for_inference(self.input_dir, self.output_dir)
+        main_inference.process_folder_for_inference(self.input_dir, self.output_dir)
+        # Check that mocked function was called with the correct arguments
+        mock_process_csv_file_for_inference.mock_calls[0].assert_called_once_with(self.test_file_path, self.test_output_file_path)
+        mock_process_csv_file_for_inference.mock_calls[1].assert_called_once_with(self.test_file_path_2, self.test_output_file_path_2)
 
-        # Check that the output file was created and has the correct content
-        output_file_path = os.path.join(self.output_dir, 'predicted_test.csv')
-        self.assertTrue(os.path.exists(output_file_path))
-        output_data = pd.read_csv(output_file_path, encoding='SHIFT_JIS')
-        self.assertEqual(output_data.shape, (3, 11))
-        self.assertListEqual(list(output_data.columns), ['計算対象','日付','内容','金額（円）','保有金融機関','大項目','中項目','メモ','振替','ID','予測_割り勘対象'])
-        self.assertListEqual(list(output_data['予測_割り勘対象']), [0, 0, 0])
+    @patch.object(main_inference, 'process_stream_for_inference')
+    @patch('builtins.open', new_callable=mock_open)
+    def test_process_csv_file_for_inference(self, mocked_open, mock_process_stream_for_inference):
+        # Prepare input and output stream as magic mocks returned by mocked_open
+        mock_input_stream = mocked_open.return_value
+        mock_output_stream = mocked_open.return_value
+        # Call the function with the test input and output files
+        main_inference.process_csv_file_for_inference(self.test_file_path, self.test_output_file_path)
+        # Check that the mocked function was called with the correct arguments
+        mocked_open.mock_calls[0].assert_called_once_with(self.test_file_path)
+        mocked_open.mock_calls[1].assert_called_once_with(self.test_output_file_path, 'w')
+        mock_process_stream_for_inference.assert_called_with(mock_input_stream, mock_output_stream)
 
+    # def test_process_stream_for_inference(self):
+    #     # Prepare the input and output streams
+    #     input_stream = open(self.test_file_path)
+    #     output_stream = tempfile.NamedTemporaryFile(mode='w')
+    #     # Load the model and label encoders
+    #     clf = main_inference.load_model()
+    #     label_encoder = main_inference.load_label_encoders()
+    #     # Call the function with the test input and output directories
+    #     main_inference.process_stream_for_inference(input_stream, output_stream, clf, label_encoder)
+    #     # Check that the output file was created and has the correct content
+    #     output_file_path = os.path.join(self.output_dir, 'predicted_test.csv')
+    #     self.assertTrue(os.path.exists(output_file_path))
+    #     output_data = pd.read_csv(output_file_path, encoding='SHIFT_JIS')
+    #     self.assertEqual(output_data.shape, (3, 11))
+    #     self.assertListEqual(list(output_data.columns), ['計算対象','日付','内容','金額（円）','保有金融機関','大項目','中項目','メモ','振替','ID','予測_割り勘対象'])
+    #     self.assertListEqual(list(output_data['予測_割り勘対象']), [0, 0, 0])
 
-    def test_process_csv_for_inference(self):
-        # Call the function with the test input and output directories
-        output_data = process_csv_for_inference(self.test_file_path)
-
-        # Check that the output file was created and has the correct content
-        self.assertEqual(output_data.shape, (3, 11))
-        self.assertListEqual(list(output_data.columns), ['計算対象','日付','内容','金額（円）','保有金融機関','大項目','中項目','メモ','振替','ID','予測_割り勘対象'])
-        self.assertListEqual(list(output_data['予測_割り勘対象']), [0, 0, 0])
 
 if __name__ == '__main__':
     unittest.main()
