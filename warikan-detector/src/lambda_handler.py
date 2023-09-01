c9a52438aefbc5e86ea826b443728d814681d9f1
Masayuki Anekawa <anekawamasayuki@gmail.com>
2023-09-01 11:38:50 +0900

    use TextIOWrapper

--- warikan-detector/src/lambda_handler.py
+++ warikan-detector/src/lambda_handler.py
@@ -2,7 +2,7 @@ import json
 import boto3
 import logging
 import os
-from io import BytesIO
+from io import BytesIO, TextIOWrapper
 
 from src.main_inference import process_stream_for_inference
 
@@ -19,23 +19,21 @@ def lambda_handler(event, context):
     logger.info(f'event: {event}')
     logger.info(f'context: {context}')
 
-    # get s3 object
+    # extract 'encoding' from env
+    encoding = os.environ.get('ENCODING', 'cp932')
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
-    # get object
     response = s3.get_object(Bucket=bucket, Key=key)
-    # get body
-    input_stream = response['Body']
-    # create output stream from string io
-    output_stream = BytesIO()
-    # process
-    process_stream_for_inference(input_stream, output_stream, )
-    # upload to s3 where bucket and key is retrieved from env values
-    output_bucket = os.environ.get('OUTPUT_BUCKET', 'warikan-detector-output', model_save_path='models/xgboost_model.pkl')
+    input_stream = TextIOWrapper(response['Body'], encoding=encoding)
+    output_stream = TextIOWrapper(ByteIO(), encoding=encoding)
+
+    process_stream_for_inference(input_stream, output_stream, model_save_path='models/xgboost_model.pkl')
+
+    output_bucket = os.environ.get('OUTPUT_BUCKET', 'warikan-detector-output')
     s3.put_object(Bucket=output_bucket, Key=key, Body=output_stream)
     response = {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': f'Successfully processed {key} in {bucket} and saved as {key} in {output_bucket}'
     }
     logger.info(f'response: {response}')
     return response
