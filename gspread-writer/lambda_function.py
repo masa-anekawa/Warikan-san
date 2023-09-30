701e74af1698c3ef4dad9b2424a35ff40d9aa238
Masayuki Anekawa <anekawamasayuki@gmail.com>
2023-09-30 14:30:38 +0900

    pass test

--- gspread-writer/lambda_function.py
+++ gspread-writer/lambda_function.py
@@ -2,6 +2,7 @@ import os
 import boto3
 import gspread
 import pandas as pd
+import pprint
 import logging
 
 from io import BytesIO
@@ -24,13 +25,15 @@ COLUMN_DEFAULT_VALUE = {
     '重複？': 'FALSE'
 }
 
+
 def lambda_handler(event, context):
     # S3からオブジェクトを取得
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
 
-    df = load_df_from_s3(s3, bucket, key, encoding=ENCODING)
-    append_df_to_gspread(df)
+    input_df = load_df_from_s3(s3, bucket, key, encoding=ENCODING)
+    import pprint; pprint.pprint(input_df)
+    append_df_to_gspread(input_df)
 
     response = {
         'statusCode': 200,
@@ -54,22 +57,61 @@ def load_df_from_s3(s3, bucket_name, file_key, encoding='utf-8'):
     return df
 
 
-def append_df_to_gspread(df: pd.DataFrame) -> None:
+def append_df_to_gspread(input_df: pd.DataFrame) -> None:
     # Google Spreadsheetに接続
     gc = gspread.service_account()
     sheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1j-7OXWMZ_GYTZ6UR1LUo5ro4px5ArHSRw7YYiZ4RHMY/edit#gid=458319299").worksheet("マスタ")
     existing_data = sheet.get_all_records()
     existing_df = pd.DataFrame(existing_data)
+
+    output_df = _calc_output_df(input_df, existing_df)
+
+    # find the range to be updated
+    first_update_row = existing_df.index[-1] + 2
+    first_col = existing_df.columns.get_loc('ID')
+    last_update_row = first_update_row + len(output_df) - 1
+    last_col = existing_df.columns.get_loc('重複？')
+
+    update_range_str = f'{_convert_index_to_alphabet(first_col)}{first_update_row}:{_convert_index_to_alphabet(last_col)}{last_update_row}'
+
+    # call batch_update
+    sheet.batch_update([
+        {
+            'range': update_range_str,
+            'values': output_df.to_dict('split')['data']
+        }
+    ])
+
+
+def _calc_output_df(input_df: pd.DataFrame, existing_df: pd.DataFrame) -> pd.DataFrame:
+    output_df = input_df.copy()
+    pprint.pprint(output_df)
+
     # IDカラムをインクリメント
-    assert(not existing_df.empty, 'Existing data is empty')
     last_id = existing_df['ID'].max()
-    df['ID'] = range(last_id + 1, last_id + 1 + len(df))
+    output_df['ID'] = range(last_id + 1, last_id + 1 + len(input_df))
+    pprint.pprint(output_df)
 
     # カラム名のマッチングとデフォルト値の追記
-    for col in existing_df.columns:
-        if col not in df.columns:
-            df[col] = 'Default_Value'  # ここで指定するデフォルト値
+    for col, default_value in COLUMN_DEFAULT_VALUE.items():
+        if col not in output_df.columns:
+            output_df[col] = default_value
+    pprint.pprint(output_df)
 
-    # 新しいデータをGoogle Spreadsheetに追記
-    for _, row in df.iterrows():
-        sheet.append_row(row.tolist())
+    # 「まさ負担額」、「まな負担額」は行ごとの計算が必要
+    output_df['まさ負担額'] = output_df['金額'] * output_df['まさ比率'] // (output_df['まさ比率'] + output_df['まな比率'])
+    output_df['まな負担額'] = output_df['金額'] - output_df['まさ負担額']
+
+    # Rearrange columns so that it matches the order of existing_df
+    output_df = output_df[existing_df.columns]
+
+    return output_df
+
+
+def _convert_index_to_alphabet(index: int) -> str:
+    """Converts 0-based index to alphabet, e.g. 0 -> A, 1 -> B, 26 -> AA, 27 -> AB"""
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if index < len(alphabet):
+        return alphabet[index]
+    else:
+        return _convert_index_to_alphabet(index // len(alphabet) - 1) + alphabet[index % len(alphabet)]
--- gspread-writer/lambda_function_test.py
+++ gspread-writer/lambda_function_test.py
@@ -24,56 +24,45 @@ class TestLambdaFunction(unittest.TestCase):
     ENCODING = 'cp932'
     INPUT_BYTES = 'input content\n'.encode(ENCODING)
     OUTPUT_BYTES = 'output content\n'.encode(ENCODING)
-    GSPREAD_RECORDS = pd.read_csv('./gspread_format.csv').to_dict()
+    EXISTING_GSPREAD_RECORDS = pd.read_csv('./gspread_format.csv').to_dict()
+    INPUT_DF = pd.read_csv('./input_format.csv')
+
+    mock_s3 = None
+    mock_gspread = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_s3 = patch.object(lambda_function, 's3').start()
+        cls.mock_gspread = patch.object(lambda_function, 'gspread').start()
+
+    @classmethod
+    def tearDownClass(cls):
+        patch.stopall()
 
     def setUp(self):
-        self.mock_s3 = patch.object(lambda_function, 's3').start()
-        self.mock_gspread_service_account = patch.object(lambda_function.gspread, 'service_account').start()
         self.mock_sheet = self._mock_gspread_records()
 
     def tearDown(self):
-        patch.stopall()
+        self.mock_sheet.reset_mock()
 
     def test_lambda_handler_writes_to_gspread_with_correct_arguments(self):
         # Set up mock S3 response
-        self._mock_s3_body(self.INPUT_BYTES)
-        mock_sheet = self._mock_gspread_records()
+        self._mock_input()
 
         # Call lambda_handler function
         lambda_function.lambda_handler(self.EVENT, None)
 
         # Assert that S3 client was called with correct arguments
         self.mock_s3.get_object.assert_called_once_with(Bucket='test-bucket', Key='test-key')
-        mock_sheet.batch_update.assert_called_once_with([
+        self.mock_sheet.batch_update.assert_called_once_with([
             {
-                'range': 'A2:M2',
-                'values': [[656,1490,'2023/8/26','まさ','ガジェット','FALSE','FALSE',2,1,8643,4322,'FALSE','FALSE']]
-            },
-            {
-                'range': 'A3:M3',
-                'values': [[657,489,'2023/8/25','まさ','ドラッグストア','FALSE','FALSE',2,1,8643,4322,'FALSE','FALSE']]
+                'range': 'A2:M3',
+                'values': [
+                    [656,1490,'2023/8/26','まさ','ガジェット','FALSE','FALSE',2,1,993,497,'FALSE','FALSE'],
+                    [657,489,'2023/8/25','まさ','ドラッグストア','FALSE','FALSE',2,1,326,163,'FALSE','FALSE']
+                ]
             }
         ])
-
-    def test_lambda_handler_handles_input_and_output_with_s3(self):
-        # Set up mock S3 response
-        body_content = BytesIO(self.INPUT_BYTES)
-        mock_response = {
-            'Body': body_content
-        }
-        self.mock_s3.get_object.return_value = mock_response
-
-        # mock transform_df
-        def mock_transform_df(input_df):
-            return pd.read_csv(BytesIO(self.OUTPUT_BYTES))
-        lambda_function.transform_df = mock_transform_df
-
-        # Call lambda_handler function
-        lambda_function.lambda_handler(self.EVENT, None)
-
-        # Assert that S3 client was called with correct arguments
-        self.mock_s3.get_object.assert_called_once_with(Bucket='test-bucket', Key='test-key')
-        self.mock_s3.put_object.assert_not_called()
 
     def _mock_s3_body(self, body_bytes: bytes):
         body_content = BytesIO(body_bytes)
@@ -82,9 +71,14 @@ class TestLambdaFunction(unittest.TestCase):
         }
         self.mock_s3.get_object.return_value = mock_response
 
+    def _mock_input(self):
+        # Prepare bytes that is read from input_format.csv, and call _mock_s3_body with it
+        input_bytes = BytesIO()
+        self.INPUT_DF.to_csv(input_bytes, index=False, encoding=self.ENCODING)
+        self._mock_s3_body(input_bytes.getvalue())
+
     def _mock_gspread_records(self):
-        self.mock_gspread_service_account.open_by_url.return_value = MagicMock()
         mock_sheet = MagicMock()
-        self.mock_gspread_service_account.open_by_url.return_value.worksheet.return_value = mock_sheet
-        mock_sheet.get_all_records.return_value = self.GSPREAD_RECORDS
+        self.mock_gspread.service_account.return_value.open_by_url.return_value.worksheet.return_value = mock_sheet
+        mock_sheet.get_all_records.return_value = self.EXISTING_GSPREAD_RECORDS
         return mock_sheet
