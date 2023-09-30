e37e5f6e750e29f7a6479de08c8aca984a643588
Masayuki Anekawa <anekawamasayuki@gmail.com>
2023-09-30 20:03:37 +0900

    Deploy gspread-writer

--- warikan-infra/ecr.tf
+++ warikan-infra/ecr.tf
@@ -57,3 +57,33 @@ resource "aws_ecr_lifecycle_policy" "warikan_detector_lifecycle" {
     ]
   })
 }
+
+resource "aws_ecr_repository" "gspread_writer_repo" {
+  name                 = "${var.app_name}-gspread-writer"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "gspread_writer_lifecycle" {
+  repository = aws_ecr_repository.gspread_writer_repo.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
--- warikan-infra/main.tf
+++ warikan-infra/main.tf
@@ -3,6 +3,16 @@ provider "aws" {
 }
 
 
+module "warikan-detector" {
+  source       = "./modules/object-transformer"
+  region       = var.region
+  account_id   = var.account_id
+  project_name = var.app_name
+  name         = "warikan-detector"
+  input_bucket = aws_s3_bucket.test_bucket
+  image_uri    = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/warikan-san-warikan-detector:81849ac2e22a579b4add46a6001cb31b"
+}
+
 module "csv_formatter" {
   source       = "./modules/object-transformer"
   region       = var.region
@@ -13,12 +23,12 @@ module "csv_formatter" {
   image_uri    = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/warikan-san-csv-formatter:6040c7d72d28553000819fcf28f6dcca"
 }
 
-module "warikan-detector" {
-  source       = "./modules/object-transformer"
+module "gspread-writer" {
+  source       = "./modules/object-consumer"
   region       = var.region
   account_id   = var.account_id
   project_name = var.app_name
-  name         = "warikan-detector"
-  input_bucket = aws_s3_bucket.test_bucket
-  image_uri    = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/warikan-san-warikan-detector:81849ac2e22a579b4add46a6001cb31b"
+  name         = "gspread-writer"
+  input_bucket = module.csv_formatter.output_bucket
+  image_uri    = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/warikan-san-gspread-writer:e7dff997ead284a625fc6cdb19649530"
 }
