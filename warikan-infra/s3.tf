6b1facd4cdac6ad84ab0101ff62bbd93b1dec519
Masayuki Anekawa <anekawamasayuki@gmail.com>
2023-12-30 23:41:28 +0900

    Add test bucket for web

--- warikan-infra/s3.tf
+++ warikan-infra/s3.tf
@@ -16,6 +16,36 @@ resource "aws_s3_bucket_acl" "initial_bucket_acl" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "${var.app_name}-test-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "test_bucket_ownership_control" {
+  bucket = aws_s3_bucket.test_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "test_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.test_bucket_ownership_control]
+
+  bucket = aws_s3_bucket.test_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_cors_configuration" "test_bucket" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST", "DELETE"]
+    allowed_origins = ["http://localhost:3000"]  # Reactのデフォルトのポートを使用している場合
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
 resource "aws_s3_bucket" "confirmed_bucket" {
   bucket = "${var.app_name}-confirmed-bucket"
 }
